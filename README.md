f16a254150cf5ecfb4f60db5baf81ac9005ed7f8
thejas <thejasb99@gmail.com>
2019-12-14 19:22:11 +0530

    Configure Net-up to take filter, network devise from env variables

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Net-up
 
-Simple application using google-gopacket to analyse network packets
+A program using google-gopacket to set BPF filter and analyse packets at a configured network devise 
 
 ## Requirement
 
@@ -9,3 +9,16 @@ Simple application using google-gopacket to analyse network packets
 ## Set-up
 
 1. `go get` the project
+2. Set the network devise to start intercepting with BPF 
+    ```
+     export NET_FILTER="tcp and port 3000" 
+     export NET_LOGLEVEL=debug 
+     export NET_NETWORKDEVISE=lo0
+     export NET_OUTPUT=stdout 
+    ```
+3. Run the executable with sudo access.
+    ```
+    sudo ./net-up
+    ```
+
+Currently the program just outputs the packet details to stdout, ie `NET_OUTPUT` supports `stdout`. It needs to be extended to support writing to file, etc.  
--- main.go
+++ main.go
@@ -1,35 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+	"strings"
 )
 
+type Config struct {
+	LogLevel string `default:"debug"`
+	NetworkDevise string `default:"eth0"`
+	Filter string `required:"true"`
+	Output string `default:"stdout"`
+}
+
+func getLogLevel(logLevel string) zapcore.Level {
+	switch strings.ToLower(logLevel) {
+		case "debug": return zapcore.DebugLevel
+		case "info": return zapcore.InfoLevel
+		case "warning": return zapcore.WarnLevel
+		case "error": return zapcore.ErrorLevel
+		default: return zapcore.InfoLevel
+	}
+}
+
+func initLogger(config Config) {
+	logger, _ := zap.Config{
+		Encoding:    "json",
+		Level:       zap.NewAtomicLevelAt(getLogLevel(config.LogLevel)),
+		OutputPaths: []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{MessageKey: "message", LevelKey: "level", EncodeLevel: zapcore.LowercaseLevelEncoder},
+	}.Build()
+	zap.ReplaceGlobals(logger)
+}
+
 func main() {
-	println("Net-up!")
-	devices, err := pcap.FindAllDevs()
+	var config Config
+	err := envconfig.Process("NET", &config)
 	if err != nil {
-		fmt.Printf("Error get devises: %s", err.Error())
+		log.Fatalf("error reading environment variable: %s", err.Error())
+	}
+	initLogger(config)
+	device := config.NetworkDevise
+	zap.S().Infof("Net-up is up and ready to capture packets at devise: %s", device)
+
+	handle, err := pcap.OpenLive(device, 1024, false, pcap.BlockForever)
+	if err != nil {
+		zap.S().Errorf("error opening handler to devise: %s", err.Error())
 		os.Exit(1)
 	}
-	device := devices[0].Name
-	handle, err := pcap.OpenLive(device, 1024, false, 30*time.Second)
-	if err != nil {
-		fmt.Printf("Error capturing packets: %s", err.Error())
-	}
-	fmt.Printf("capturing packets for: %s \n", device)
-	filter := "tcp and port 3000"
+	filter := config.Filter
+	zap.S().Infof("Setting up BPF filter: %s", filter)
+
 	err = handle.SetBPFFilter(filter)
 	if err != nil {
-		fmt.Printf("Error applying filter: %s", err.Error())
+		zap.S().Errorf("Error applying filter: %s", err.Error())
 	}
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		fmt.Println(packet)
+	mode := config.Output
+	switch strings.ToLower(mode) {
+		case "stdout": outputToStdOutput(packetSource)
+		default:
+			zap.S().Fatalf("Invalid output type, currently supported `stdout`")
+	}
+}
+
+func outputToStdOutput(source *gopacket.PacketSource) {
+	for packet := range source.Packets() {
+		zap.S().Info(packet.String())
 	}
 
 }
